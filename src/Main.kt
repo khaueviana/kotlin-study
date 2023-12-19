adbb04da17be1b258937c82313471e6b6cb20ce8
Khauê Viana <khaue.viana@farfetch.com>
2023-12-19 17:11:39 +0000

    hello kotlin

---
+++ src/Main.kt
@@ -0,0 +1,18 @@
+val bacalhau = 1
+
+fun main(){
+    println("Hello, world!")
+
+    // Variables
+    val popcorn = 5 // read only
+    val bifanas = 7 // mutable
+    var customers = 10
+
+    customers = 8
+    println(popcorn)
+    println(bifanas)
+    println(customers)
+    println(bacalhau)
+
+    //
+}
